6dcae0f73b42a3489596a9684bc1dd1fa62b1012
Shynamo <broseau@gmail.com>
2024-01-24 22:25:24 +0100

    nvim: Update neo_tree config

--- config/nvim/lua/baptiste/plugins/neo_tree.lua
+++ config/nvim/lua/baptiste/plugins/neo_tree.lua
@@ -18,65 +18,8 @@ return {
             { text = "󰌵", texthl = "DiagnosticSignHint" })
 
         require("neo-tree").setup({
-            close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
             popup_border_style = "rounded",
-            enable_git_status = true,
-            enable_diagnostics = true,
-            enable_normal_mode_for_inputs = false,                             -- Enable normal mode for input dialogs.
-            open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-            sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
-
             default_component_configs = {
-                container = {
-                    enable_character_fade = true
-                },
-                indent = {
-                    indent_size = 2,
-                    padding = 1, -- extra padding on left hand side
-                    -- indent guides
-                    with_markers = true,
-                    indent_marker = "│",
-                    last_indent_marker = "└",
-                    highlight = "NeoTreeIndentMarker",
-                    -- expander config, needed for nesting files
-                    with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-                    expander_collapsed = "",
-                    expander_expanded = "",
-                    expander_highlight = "NeoTreeExpander",
-                },
-                icon = {
-                    folder_closed = "",
-                    folder_open = "",
-                    folder_empty = "󰜌",
-                    -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
-                    -- then these will never be used.
-                    default = "*",
-                    highlight = "NeoTreeFileIcon"
-                },
-                modified = {
-                    symbol = "[+]",
-                    highlight = "NeoTreeModified",
-                },
-                name = {
-                    trailing_slash = false,
-                    use_git_status_colors = true,
-                    highlight = "NeoTreeFileName",
-                },
-                git_status = {
-                    symbols = {
-                        -- Change type
-                        added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-                        modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                        deleted   = "✖", -- this can only be used in the git_status source
-                        renamed   = "󰁕", -- this can only be used in the git_status source
-                        -- Status type
-                        untracked = "",
-                        ignored   = "",
-                        unstaged  = "󰄱",
-                        staged    = "",
-                        conflict  = "",
-                    }
-                },
                 -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
                 file_size = {
                     enabled = true,
@@ -103,7 +46,13 @@ return {
                 force_visible_in_empty_folder = true, -- when true, hidden files will be shown if the root folder is otherwise empty
                 show_hidden_count = true,             -- when true, the number of hidden items in each folder will be shown as the last entry
                 hide_dotfiles = false,
-                hide_gitignored = true,
+                hide_gitignored = false,
+            },
+            find_by_full_path_words = true,
+            follow_current_file = {
+                enabled = false,         -- This will find and focus the file in the active buffer every time
+                -- the current file is changed while the tree is open.
+                leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
             },
         })
     end
