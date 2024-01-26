b3df7edb59c32783b86daa62f5a866a7a618ae5f
Baptiste Roseau <broseau@gmail.com>
2024-01-26 17:34:07 +0100

    nvim: Move powerline->lualine

---
+++ config/nvim/lua/baptiste/plugins/lualine.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"ryanoasis/vim-devicons",
+		"rmagatti/auto-session",
+	},
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "vscode",
+		},
+		sections = { lualine_c = { require("auto-session.lib").current_session_name } },
+	},
+	priority = 100,
+}
