5fc08f557fd5550d06c7bd7110926ce508fb9154
Baptiste Roseau <broseau@gmail.com>
2024-02-27 12:08:06 +0100

    nvim: update debug/info/warn/error line background colors

--- config/nvim/lua/baptiste/core/highlight.lua
+++ config/nvim/lua/baptiste/core/highlight.lua
@@ -11,9 +11,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.cmd.highlight({ "Blamer", "guifg=#696969" })
 
 -- Diagnostics line background
-vim.api.nvim_set_hl(0, "DiagnosticHintLn", { bg = "#1e1e2e" })
-vim.api.nvim_set_hl(0, "DiagnosticInfoLn", { bg = "#1e1e2e"})
-vim.api.nvim_set_hl(0, "DiagnosticErrorLn", { bg ="#2e1e1e" })
+vim.api.nvim_set_hl(0, "DiagnosticHintLn", { bg = "#1e1e37" })
+vim.api.nvim_set_hl(0, "DiagnosticInfoLn", { bg = "#1e1e37"})
+vim.api.nvim_set_hl(0, "DiagnosticErrorLn", { bg ="#371e1e" })
 vim.api.nvim_set_hl(0, "DiagnosticWarnLn", { bg = "#2e2e1e" })
 vim.diagnostic.config({
 	signs = { linehl = { "DiagnosticErrorLn", "DiagnosticWarnLn", "DiagnosticInfoLn", "DiagnosticHintLn" } },
