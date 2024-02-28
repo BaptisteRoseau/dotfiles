4c10aca1738e5381bb6fd17b4c436847d0a0ee8e
Baptiste Roseau <broseau@gmail.com>
2024-02-28 17:29:21 +0100

    nvim: Updated highlights

--- config/nvim/lua/baptiste/core/highlight.lua
+++ config/nvim/lua/baptiste/core/highlight.lua
@@ -8,16 +8,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Current Line
-vim.cmd.highlight({ "CursorLine", "guibg=#2C2C2C" })
+vim.cmd.highlight({ "CursorLine", "guibg=#292929" })
 
 -- Blamer
 vim.cmd.highlight({ "Blamer", "guifg=#696969" })
 
 -- Diagnostics line background
-vim.api.nvim_set_hl(0, "DiagnosticHintLn", { bg = "#1e1e3A" })
-vim.api.nvim_set_hl(0, "DiagnosticInfoLn", { bg = "#1e1e3A" })
-vim.api.nvim_set_hl(0, "DiagnosticWarnLn", { bg = "#2e2e20" })
-vim.api.nvim_set_hl(0, "DiagnosticErrorLn", { bg = "#3A1e1e" })
+vim.api.nvim_set_hl(0, "DiagnosticErrorLn", { bg = "#3a1e1e" })
+vim.api.nvim_set_hl(0, "DiagnosticWarnLn", { bg = "#3a3a1e" })
+vim.api.nvim_set_hl(0, "DiagnosticInfoLn", { bg = "#1e1e3a" })
+vim.api.nvim_set_hl(0, "DiagnosticHintLn", { bg = "#1e1e3a" })
 vim.diagnostic.config({
     signs = { linehl = { "DiagnosticHintLn", "DiagnosticInfoLn", "DiagnosticWarnLn", "DiagnosticErrorLn" } },
 })
