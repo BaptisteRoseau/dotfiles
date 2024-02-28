b167b638c048568286d6919c78021e3c481b1793
Baptiste Roseau <broseau@gmail.com>
2024-02-28 15:30:40 +0100

    nvim: Fix history plugin

--- config/nvim/lua/baptiste/plugins/history.lua
+++ config/nvim/lua/baptiste/plugins/history.lua
@@ -1,13 +1,14 @@
 return {
     "wilfreddenton/history.nvim",
-    opts = {
-        keybinds = {
-            back = "<M-Left>",
-            forward = "<M-Right>",
-            view = "<leader>bH",
-        },
-    },
+    opts = {},
     config = function()
+        require("history").setup({
+            keybinds = {
+                back = "<M-Left>",
+                forward = "<M-Right>",
+                view = "<leader>bH",
+            },
+        })
         require("which-key").register({
             ["<leader>bH"] = { name = "[B]uffer [H]istory", _ = "which_key_ignore" },
             ["<leader>b"] = { name = "[B]uffer", _ = "which_key_ignore" },
