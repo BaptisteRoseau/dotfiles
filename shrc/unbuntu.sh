82cdda78d51c8aa550056bc92310a5fc1630b245
Baptiste Roseau <broseau@gmail.com>
2024-03-01 12:57:11 +0100

    shrc: Add git branch on PS1

--- shrc/unbuntu.sh
+++ shrc/unbuntu.sh
@@ -20,9 +20,9 @@ if [ -n "$force_color_prompt" ]; then
     fi
 fi
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(_git_where)\$ '
 else
-    PS1='${debian_chroot:+($debian_chroot)}\u:\w\$ '
+    PS1='${debian_chroot:+($debian_chroot)}\u:\w$(_git_where)\$ '
 fi
 unset color_prompt force_color_prompt
 case "$TERM" in
