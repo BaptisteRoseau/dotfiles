663dac5063dad9f11f6c0d49544557fa6178956d
Baptiste Roseau <broseau@gmail.com>
2024-03-01 12:46:02 +0100

    nvim: Change CWD based on CLI's argument

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,17 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- TODO: Find or write a plugin to add special characters at the
+-- begining and the end of a visual selection as in VSCode
+
+-- When opening a directory with `nvim /path/to/directory`,
+-- make CWD to be that directory. This fixes
+-- Telescope's `find_files` to search within /path/to/directory
+-- instead of the directory where the nvim command has been executed.
+local arg = vim.fn.expand('%:p')
+if vim.fn.isdirectory(arg) == 1 then
+  vim.api.nvim_set_current_dir(arg)
+end
+
 require("baptiste.lazy")
 require("baptiste.core")
