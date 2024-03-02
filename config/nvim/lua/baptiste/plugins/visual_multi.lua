37aff27a27d758abafcc115b1f199a9296845729
Baptiste Roseau <broseau@gmail.com>
2024-03-02 18:07:49 +0100

    nvim: Fix visual-multi

--- config/nvim/lua/baptiste/plugins/visual_multi.lua
+++ config/nvim/lua/baptiste/plugins/visual_multi.lua
@@ -1,16 +1,17 @@
 -- Should be written BEFORE the plugin is initialized
 -- https://github.com/mg979/vim-visual-multi/wiki/Mappings#full-mappings-list
 -- See available mappings here:
--- vim.g.VM_maps = {}
--- vim.g.VM_maps["Find Under"] = "<C-d>"
--- vim.g.VM_maps["Find Subword Under"] = "<C-d>"
--- vim.g.VM_maps["Select Cursor Down"] = "<C-S-Down>"
--- vim.g.VM_maps["Select Cursor Up"] = "<C-S-Up>"
--- vim.g.VM_maps["Mouse Cursor"] = "<M-LeftMouse>"
--- vim.g.VM_maps["Select All"] = "<C-L>"
--- vim.g.VM_maps["Visual All"] = "<C-L>"
--- vim.g.VM_maps["Undo"] = "u"
--- vim.g.VM_maps["Redo"] = "<C-r>"
+vim.g.VM_maps = {
+    ["Find Under"] = "<C-d>",
+    ["Find Subword Under"] = "<C-d>",
+    ["Select Cursor Down"] = "<C-S-Down>",
+    ["Select Cursor Up"] = "<C-S-Up>",
+    ["Mouse Cursor"] = "<M-LeftMouse>",
+    ["Select All"] = "<C-L>",
+    ["Visual All"] = "<C-L>",
+    ["Undo"] = "u",
+    ["Redo"] = "<C-r>",
+}
 vim.g.VM_highlight_matches = "hi! Search cterm=NONE guibg=#303030"
 vim.g.VM_theme = "codedark"
 
