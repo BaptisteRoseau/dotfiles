1157401db6ed080fdb6b5f0b73622551a312d755
Baptiste Roseau <broseau@gmail.com>
2024-03-14 11:52:27 +0100

    nvim: Change crates shortcut prefix

--- config/nvim/lua/baptiste/plugins/crates.lua
+++ config/nvim/lua/baptiste/plugins/crates.lua
@@ -5,25 +5,25 @@ return {
 		require("crates").setup({
 			on_attach = function(_)
 				local crates = require("crates")
-				vim.keymap.set("n", "<leader>cr", crates.reload, { desc = "[C]rates [R]eload" })
+				vim.keymap.set("n", "<leader>Cr", crates.reload, { desc = "[C]rates [R]eload" })
 
-				vim.keymap.set("n", "<leader>cv", crates.show_versions_popup, { desc = "[C]rate [V]ersions" })
-				vim.keymap.set("n", "<leader>cf", crates.show_features_popup, { desc = "[C]rate [F]eatures" })
-				vim.keymap.set("n", "<leader>cd", crates.show_dependencies_popup, { desc = "[C]rate [D]ependencies" })
+				vim.keymap.set("n", "<leader>Cv", crates.show_versions_popup, { desc = "[C]rate [V]ersions" })
+				vim.keymap.set("n", "<leader>Cf", crates.show_features_popup, { desc = "[C]rate [F]eatures" })
+				vim.keymap.set("n", "<leader>Cd", crates.show_dependencies_popup, { desc = "[C]rate [D]ependencies" })
 
-				vim.keymap.set("n", "<leader>cu", crates.update_crate, { desc = "[C]rate [U]pdate" })
-				vim.keymap.set("v", "<leader>cu", crates.update_crates, { desc = "[C]rates [U]pdate" })
-				vim.keymap.set("n", "<leader>ca", crates.update_all_crates, { desc = "[C]rates update [A]ll" })
-				vim.keymap.set("n", "<leader>cU", crates.upgrade_crate, { desc = "[C]rate [U]pgrade" })
-				vim.keymap.set("v", "<leader>cU", crates.upgrade_crates, { desc = "[C]rates [U]pgrade" })
-				vim.keymap.set("n", "<leader>cA", crates.upgrade_all_crates, { desc = "[C]rates upgrade [A]ll" })
+				vim.keymap.set("n", "<leader>Cu", crates.update_crate, { desc = "[C]rate [U]pdate" })
+				vim.keymap.set("v", "<leader>Cu", crates.update_crates, { desc = "[C]rates [U]pdate" })
+				vim.keymap.set("n", "<leader>Ca", crates.update_all_crates, { desc = "[C]rates update [A]ll" })
+				vim.keymap.set("n", "<leader>CU", crates.upgrade_crate, { desc = "[C]rate [U]pgrade" })
+				vim.keymap.set("v", "<leader>CU", crates.upgrade_crates, { desc = "[C]rates [U]pgrade" })
+				vim.keymap.set("n", "<leader>CA", crates.upgrade_all_crates, { desc = "[C]rates upgrade [A]ll" })
 
-				vim.keymap.set("n", "<leader>cH", crates.open_homepage, { desc = "[C]rate [H]omepage" })
-				vim.keymap.set("n", "<leader>cR", crates.open_repository, { desc = "[C]rate [R]epository" })
-				vim.keymap.set("n", "<leader>cD", crates.open_documentation, { desc = "[C]rate [D]documentation" })
-				vim.keymap.set("n", "<leader>cC", crates.open_crates_io, { desc = "[C]rate open [C]rates.io" })
+				vim.keymap.set("n", "<leader>CH", crates.open_homepage, { desc = "[C]rate [H]omepage" })
+				vim.keymap.set("n", "<leader>CR", crates.open_repository, { desc = "[C]rate [R]epository" })
+				vim.keymap.set("n", "<leader>CD", crates.open_documentation, { desc = "[C]rate [D]documentation" })
+				vim.keymap.set("n", "<leader>CC", crates.open_crates_io, { desc = "[C]rate open [C]rates.io" })
 				require("which-key").register({
-					["<leader>c"] = { name = "[C]rates", _ = "which_key_ignore" },
+					["<leader>C"] = { name = "[C]rates", _ = "which_key_ignore" },
 				})
 			end,
 		})
